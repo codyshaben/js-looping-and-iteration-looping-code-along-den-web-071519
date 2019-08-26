d19ae59034b95ff26aae72356a16c9e92482785f
Cody Shaben <codyshaben@Codys-MacBook-Air.local>
2019-08-26 17:21:35 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+let names = ["Lisa", "Kaitlin","Jan"];
+const event = "surprise"
+
+function writeCards(names, event) {
+    let thankYouCards = []
+    for (let i = 0; i < names.length; i++) {
+        thankYouCards.push( `Thank you, ${names[i]}, for the wonderful ${event} gift!` )
+    }
+    return thankYouCards
+}
+
+function countdown(countdown) {
+    while ( countdown > 0) {
+        console.log(countdown);
+        countdown -= 1;
+    }
+    console.log(countdown);
+}
+ 
+
+
+
